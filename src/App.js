aeb44a36bf267435ef8733d9bd8f65c29355afa0
Felix Ajibola <oguntomilola.felix@adexfe.com>
2023-09-15 00:04:00 +0100

    Update Router in App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import Navbar from "./Navbar";
 import Home from "./Home";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Create from "./Create";
 
 function App() {
   const title = "Welcome to my app";
   const likes = 50;
 
   return (
-    <div className="App">
-      <Navbar />
-
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
