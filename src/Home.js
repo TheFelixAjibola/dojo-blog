d1e95b4e86739fe5236e281c21f53906b627eb62
Felix Ajibola <oguntomilola.felix@adexfe.com>
2023-09-14 22:53:25 +0100

    Enable error handling function

--- src/Home.js
+++ src/Home.js
@@ -4,16 +4,23 @@ import BlogList from "./BlogList";
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
   const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     setTimeout(() => {
       fetch("http://localhost:8000/blogs")
         .then((res) => {
+          if (!res.ok) {
+            throw Error("It could not fetch the data for that resource.");
+          }
           return res.json();
         })
         .then((data) => {
           setBlogs(data);
           setIsPending(false);
+        })
+        .catch((err) => {
+          setError(err.message);
         });
     }, 1000);
   }, []);
