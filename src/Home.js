ae2fce6473831ffc228a04438f0b4397e076737c
Felix Ajibola <oguntomilola.felix@adexfe.com>
2023-09-14 22:59:44 +0100

    updated Error Handling

--- src/Home.js
+++ src/Home.js
@@ -11,15 +11,17 @@ const Home = () => {
       fetch("http://localhost:8000/blogs")
         .then((res) => {
           if (!res.ok) {
-            throw Error("It could not fetch the data for that resource.");
+            throw Error("Could not fetch the data for that resource.");
           }
           return res.json();
         })
         .then((data) => {
           setBlogs(data);
           setIsPending(false);
+          setError(null);
         })
         .catch((err) => {
+          setIsPending(false);
           setError(err.message);
         });
     }, 1000);
@@ -27,6 +29,7 @@ const Home = () => {
 
   return (
     <div className="home">
+      {error && <div>{error}</div>}
       {isPending && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
     </div>
