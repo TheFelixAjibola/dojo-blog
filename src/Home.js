e0e03b004c07e95ca88dda49bc9110d7fcef5ef1
Felix Ajibola <oguntomilola.felix@adexfe.com>
2023-09-14 21:11:26 +0100

    Used React to Fetch Local API by starting json

---
+++ src/Home.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+import BlogList from "./BlogList";
+
+const Home = () => {
+  const [blogs, setBlogs] = useState(null);
+
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
+  };
+
+  useEffect(() => {
+    fetch("http://localhost:8000/blogs")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setBlogs(data);
+      });
+  }, []);
+
+  return (
+    <div className="home">
+      <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
+    </div>
+  );
+};
+
+export default Home;
