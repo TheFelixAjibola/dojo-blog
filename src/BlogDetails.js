e68189cbf4c51b9073508470e13d4b04bedb6ef1
Felix Ajibola <oguntomilola.felix@adexfe.com>
2023-09-15 02:01:18 +0100

    Implemented the DELETE method and redirect to Home

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,13 +1,25 @@
-import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import {
+  useHistory,
+  useParams,
+} from "react-router-dom/cjs/react-router-dom.min";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
   const { id } = useParams();
+  const history = useHistory();
   const {
     data: blog,
     isPending,
     error,
   } = useFetch(`http://localhost:8000/blogs/${id}`);
+
+  const handleClick = () => {
+    fetch(`http://localhost:8000/blogs/${blog.id}`, {
+      method: "DELETE",
+    }).then(() => {
+      history.push("/");
+    });
+  };
 
   return (
     <div className="blog-details">
@@ -18,6 +30,7 @@ const BlogDetails = () => {
           <h2>{blog.title}</h2>
           <p>Written by {blog.author}</p>
           <div>{blog.body}</div>
+          <button onClick={handleClick}>Delete</button>
         </article>
       )}
     </div>
